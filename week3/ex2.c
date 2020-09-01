45f9bfbb24719e78e4fd97858fcc3ad6831252e8
romanza705 <r.mukhtarov@innopolis.university>
2020-09-01 17:51:34 +0300

    coment error kak ze horosho chto est local history

--- week3/ex2.c
+++ week3/ex2.c
@@ -1,4 +1,39 @@
 //
 // Created by roman on 01.09.2020.
 //
+#include <stdio.h>
+
+
+void bubble_sort(int *arr, int n){
+    for (int i = 0 ; i < n - 1; i++)
+    {
+        for (int j = 0 ; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                arr[j+1] = arr[j] + arr[j+1];
+                arr[j] = arr[j+1] - arr[j];
+                arr[j+1] = arr[j+1] - arr[j];
+            }
+        }
+    }
+}
+int main()
+{
+    int n, d, swap;
+    printf("Number of elements:\n");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    bubble_sort(arr, n);
+    printf("Sorted list:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d\t", arr[i]);
+
+    return 0;
+}
+
+
 
